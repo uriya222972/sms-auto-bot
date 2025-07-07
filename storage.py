b885f598ce514befe73b67be2baa7f3dd546b074
uriya222972 <ayalado.doron@gmail.com>
2025-07-07 13:57:47 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,14 +1,14 @@
 import json
 import os
 
-DATA_FILE = "data.json"
+STORAGE_FILE = "data.json"
 
 def save_data(data):
-    with open(DATA_FILE, "w", encoding="utf-8") as f:
+    with open(STORAGE_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 def load_data():
-    if not os.path.exists(DATA_FILE):
-        return {}
-    with open(DATA_FILE, "r", encoding="utf-8") as f:
-        return json.load(f)
+    if os.path.exists(STORAGE_FILE):
+        with open(STORAGE_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return {}
